0fec0fb4ece68bcd5fd41f4b56d7c8695fd01c3a
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-01 21:16:53 +0100

    Extended rules for linter

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-      "no-restricted-syntax": "off"
+      "no-restricted-syntax": "off",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "padded-blocks": ["error", {
+        "classes": "always",
+        "switches": "never",
+        "blocks": "never",
+      }],
     }
 };
