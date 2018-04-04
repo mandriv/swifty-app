c8539bef73694e25d9e798fd5318a336ed57ee86
Martin Kollie <martinkollie@gmail.com>
2018-04-04 18:36:24 +0100

    Fix setup.py script

--- server/setup.py
+++ server/setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(name='Swifty Server',
       version='1.0',
       url='https://github.com/mandriv/swifty-app/',
-      packages=['swifty-server'],
+      packages=['server'],
       install_requires=['flask', 'flask-jwt-extended, flask_pymongo'],
 )
