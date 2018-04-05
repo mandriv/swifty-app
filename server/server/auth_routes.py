e48ed86def86efbb8bc7de6377ccff4d9fa9b842
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-04-05 03:23:27 +0000

    normal user register

--- server/server/auth_routes.py
+++ server/server/auth_routes.py
@@ -15,15 +15,15 @@ def create_account():
 @app.route('/api/auth/login', methods=['POST'])
 def login():
     if not request.is_json:
-        return jsonify({"msg": "Request must be in JSON"}), 400
+        return jsonify(msg="Request must be in JSON"), 400
 
     user = User.query.filter_by(username=request.json['username']).first()
 
     if (user == None):
-        return jsonify({"msg": "User not exist"}), 400
+        return jsonify(msg="User not exist"), 400
 
     if (not user.check_password(request.json['password'])):
-        return jsonify({"msg": "Wrong password"}), 400
+        return jsonify(msg="Wrong password"), 400
 
     access_token = create_access_token(identity=user.id)
     return jsonify(access_token=access_token, user=user.to_json()), 200
--- server/server/user_routes.py
+++ server/server/user_routes.py
@@ -1,4 +1,7 @@
-from server import app
+from server import app, db
+from server.User import User
+from flask import jsonify, request
+from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 @app.route('/api/user/<int:userID>', methods=['GET'])
@@ -7,3 +10,28 @@ def get_user(userID):
     current_user = get_jwt_identity()
     return 'User %d %s ' % (userID, current_user)
 
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    if not request.is_json:
+        return jsonify(msg="Request must be in JSON"), 400
+
+    user_data = request.get_json()
+
+    if (not 'username' in user_data
+            or not 'password' in user_data
+            or not 'email' in user_data):
+       return jsonify(msg="username, password, email fields are required!"), 400
+
+    user = User(username=user_data['username'], email=user_data['email'], role=0)
+    user.set_hash_password(user_data['password'])
+
+    if 'bio' in user_data:
+        user.bio = user_data['bio']
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except IntegrityError:
+        return jsonify(msg="username or email already in use"), 400
+
+    return jsonify(msg="user created")
