aa3ce830f1fb54c120d9fd199173a184d108345f
Martin Kollie <martinkollie@gmail.com>
2018-04-05 10:49:18 +0100

    Update setup.py and requirements.txt

--- server/setup.py
+++ server/setup.py
@@ -4,5 +4,5 @@ setup(name='Swifty Server',
       version='1.0',
       url='https://github.com/mandriv/swifty-app/',
       packages=['server'],
-      install_requires=['flask', 'flask-jwt-extended, flask_pymongo'],
-)
+      install_requires=['flask', 'flask-jwt-extended, flask_pymongo', 'SQLAlchemy'],
+      )
