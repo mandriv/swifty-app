20e35b4a7b211bdbac5ce596e959849eb4e23f7f
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-04-05 02:00:33 +0000

    using sha512 instead of 256

--- server/server/User.py
+++ server/server/User.py
@@ -5,8 +5,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    passw = db.Column(db.String(64), unique=False, nullable=False)
+    passw = db.Column(db.String(128), unique=False, nullable=False)
     bio = db.Column(db.TEXT, unique=False, nullable=True)
+    role = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -20,5 +21,5 @@ class User(db.Model):
         return self.passw == self.__hash_password(password)
 
     def __hash_password(self, password):
-        hash_object = hashlib.sha3_256(password.encode('utf-8'))
+        hash_object = hashlib.sha3_512(password.encode('utf-8'))
         return hash_object.hexdigest()
