47e34a049a386e782488311ee0550494985550bc
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 00:09:19 +0100

    Fix indentation in RegisterForm

--- app/src/screens/Register/RegisterForm/index.js
+++ app/src/screens/Register/RegisterForm/index.js
@@ -1,55 +1,61 @@
 import React from 'react';
 import { Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { white } from '../../../config/colours';
+import { white, secondaryLight } from '../../../config/colours';
 
 import Button from '../../../shared/Button';
 import styles from './styles';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class RegisterForm extends React.Component {
 
-    render() {
+  render() {
     return (
       <View style={styles.container}>
-      <View style = {styles.leftView}>
-        <Text style={styles.label}>Username</Text>
+        <View style={styles.leftView}>
+          <Text style={styles.label}>Username</Text>
         </View>
         <View style={styles.inputAndIcon}>
-       <Icon style={styles.icon} name="user" size={20} color="#F2994A"/>
-       <TextInput style={styles.input}
-       autoCorrect={false}
-       autoCapitalize='none'
-         underlineColorAndroid='#fff'
-         onSubmitEditing={() => this.emailInput.focus()}/>
-       </View>
-    <Text style={[styles.label,styles.mailLabel]}>E-mail</Text>
-        
-    <View style={styles.inputAndIcon}>
-       <Icon style={styles.icon} name="at" size={20} color="#F2994A"/>
-       <TextInput style={styles.input}
-       keyboardType='email-address'
-       autoCorrect={false}
-       autoCapitalize='none'
-         underlineColorAndroid='#fff'
-       onSubmitEditing={() => this.passwordInput.focus()}/>
-       </View>
-       <View style = {styles.leftView}>
-        <Text style={styles.label}>Password</Text>
+          <Icon style={styles.icon} name="user" size={20} color="#F2994A" />
+          <TextInput
+            style={styles.input}
+            autoCorrect={false}
+            autoCapitalize="none"
+            underlineColorAndroid="#fff"
+            onSubmitEditing={() => this.emailInput.focus()}
+          />
+        </View>
+        <Text style={[styles.label, styles.mailLabel]}>E-mail</Text>
+
+        <View style={styles.inputAndIcon}>
+          <Icon style={styles.icon} name="at" size={20} color="#F2994A" />
+          <TextInput
+            style={styles.input}
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            underlineColorAndroid="#fff"
+            onSubmitEditing={() => this.passwordInput.focus()}
+          />
+        </View>
+        <View style={styles.leftView}>
+          <Text style={styles.label}>Password</Text>
         </View>
         <View style={styles.inputAndIcon}>
-            <Icon style={styles.icon} name="lock" size={20} color="#F2994A"/>
-        <TextInput style={styles.input}
-        ref ={(input) => this.passwordInput= input}
-         underlineColorAndroid='#fff'
-        autoCorrect={false}
-       autoCapitalize='none'
-        secureTextEntry/>
+          <Icon style={styles.icon} name="lock" size={20} color="#F2994A" />
+          <TextInput
+            style={styles.input}
+            ref={input => this.passwordInput = input}
+            underlineColorAndroid="#fff"
+            autoCorrect={false}
+            autoCapitalize="none"
+            secureTextEntry
+          />
         </View>
-        <TouchableOpacity style = {styles.buttonContainer}
->
-<Text style = {styles.button}>Go</Text>
-</TouchableOpacity>
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Text style={styles.button}>Go</Text>
+        </TouchableOpacity>
       </View>
     );
   }
+
 }
