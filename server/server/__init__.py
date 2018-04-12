4ff6f946663bb347fcdba30164234a9e97bf9745
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-04-12 18:58:53 +0000

    migration imported

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -16,6 +17,7 @@ app.config['SECRET_KEY'] = 'evenmoresecretthanthis'
 # mongo = PyMongo(app, config_prefix='MONGO')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # jwt
 jwt_key = os.environ.get('JWT_SECRET_KEY')
