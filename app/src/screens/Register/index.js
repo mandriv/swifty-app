7e670aafae83b517817f3252b105a4202415e18f
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 00:08:04 +0100

    Fix indentation in Register

--- app/src/screens/Register/index.js
+++ app/src/screens/Register/index.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import { Text, View, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from './styles';
 import RegisterForm from './RegisterForm';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { white } from '../../config/colours';
+
 
 export default class Register extends React.Component {
   render() {
     return (
-      
-      <View
-      style={styles.container}>
-       <View style={styles.shape} />
+      <View style={styles.container}>
+        <View style={styles.shape} />
         <View style={styles.adjusted}>
-        <Text style = {styles.signIn}>Register</Text>
-        <View>
-</View>
-    <View style={styles.formContainer}>
-        <RegisterForm/>
+          <Text style={styles.signIn}>Register</Text>
+          <View style={styles.formContainer}>
+            <RegisterForm />
+          </View>
+          <View style={styles.social}>
+            <TouchableOpacity style={[styles.circle, styles.circleLeft]}>
+              <Icon style={styles.button} name="facebook" size={40} color={white} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.circle} >
+              <Icon style={styles.button} name="google" size={40} color={white} />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.social}>
-        <TouchableOpacity style={[styles.circle,styles.circleLeft]} >
-        <Icon style={styles.button} name="facebook" size={40} color="#FDFFFF"/>
-        </TouchableOpacity>
-         <TouchableOpacity style={styles.circle} >
-          <Icon style={styles.button} name="google" size={40} color="#FDFFFF"/>
-        </TouchableOpacity>
-          </View>
-          </View>
-          <View style={styles.shape2} />
+        <View style={styles.shape2} />
       </View>
     );
   }
+
 }
