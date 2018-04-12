9f65125bf1afebacc329b7e0f65c3b7d00504a28
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-04-12 20:39:41 +0000

    serving static files

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config.from_object(__name__)
+
+@app.route('/static/<path:path>')
+def static_file_serving(path):
+    return send_from_directory('static', path)
+
 
 # db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../sql'
