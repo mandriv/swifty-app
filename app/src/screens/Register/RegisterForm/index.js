71aa26944511f52e08c0c44a7fa258f82a0633fb
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 00:44:28 +0100

    Integrate Input in register

--- app/src/screens/Register/RegisterForm/index.js
+++ app/src/screens/Register/RegisterForm/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View } from 'react-native';
 
-import { white, secondaryLight } from '../../../config/colours';
 import styles from './styles';
 import Button from '../../../shared/Button';
+import Input from '../../../shared/Input';
 
 
 export default class RegisterForm extends React.Component {
@@ -12,46 +11,30 @@ export default class RegisterForm extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.leftView}>
-          <Text style={styles.label}>Username</Text>
-        </View>
-        <View style={styles.inputAndIcon}>
-          <Icon style={styles.icon} name="user" size={20} color={secondaryLight} />
-          <TextInput
-            style={styles.input}
-            autoCorrect={false}
-            autoCapitalize="none"
-            underlineColorAndroid={white}
-            onSubmitEditing={() => this.emailInput.focus()}
-          />
-        </View>
-        <Text style={[styles.label, styles.mailLabel]}>E-mail</Text>
-        <View style={styles.inputAndIcon}>
-          <Icon style={styles.icon} name="at" size={20} color={secondaryLight} />
-          <TextInput
-            ref={input => this.emailInput = input} // eslint-disable-line
-            style={styles.input}
-            keyboardType="email-address"
-            autoCorrect={false}
-            autoCapitalize="none"
-            underlineColorAndroid={white}
-            onSubmitEditing={() => this.passwordInput.focus()}
-          />
-        </View>
-        <View style={styles.leftView}>
-          <Text style={styles.label}>Password</Text>
-        </View>
-        <View style={styles.inputAndIcon}>
-          <Icon style={styles.icon} name="lock" size={20} color={secondaryLight} />
-          <TextInput
-            ref={input => this.passwordInput = input} // eslint-disable-line
-            style={styles.input}
-            underlineColorAndroid={white}
-            autoCorrect={false}
-            autoCapitalize="none"
-            secureTextEntry
-          />
-        </View>
+        <Input
+          label="Username"
+          icon="user"
+          autoCorrect={false}
+          autoCapitalize="none"
+          onSubmitEditing={() => this.emailInput.focus()}
+        />
+        <Input
+          ref={input => this.emailInput = input} // eslint-disable-line
+          label="E-mail"
+          icon="at"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          onSubmitEditing={() => this.passwordInput.focus()}
+        />
+        <Input
+          ref={input => this.passwordInput = input} // eslint-disable-line
+          label="Password"
+          icon="lock"
+          autoCorrect={false}
+          autoCapitalize="none"
+          secureTextEntry
+        />
         <View style={styles.button}>
           <Button type="primary" fluid>
             Go
--- app/src/screens/Register/RegisterForm/styles.js
+++ app/src/screens/Register/RegisterForm/styles.js
@@ -1,24 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { black, blackLight, primaryLight, white, rgba } from '../../../config/colours';
+import { primaryLight } from '../../../config/colours';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-  },
-  input: {
-    width: 220,
-    height: 45,
-    paddingLeft: 10,
-
-  },
-  label: {
-    fontSize: 12,
-    justifyContent: 'flex-start',
-    color: blackLight,
-    marginRight: 190,
-  },
-  mailLabel: {
-    marginRight: 210,
   },
   buttonContainer: {
     height: 45,
@@ -39,22 +24,6 @@ const styles = StyleSheet.create({
     height: 45,
     width: '80%',
   },
-  inputAndIcon: {
-    flexDirection: 'row',
-    height: 45,
-    backgroundColor: rgba(white, 0.7),
-    marginBottom: 20,
-    width: 250,
-    paddingLeft: 10,
-    borderColor: rgba(blackLight, 0.7),
-    borderWidth: 1,
-    borderRadius: 5,
-  },
-  icon: {
-    flex: 0,
-    marginTop: 10,
-  },
-
 });
 
 export default styles;
