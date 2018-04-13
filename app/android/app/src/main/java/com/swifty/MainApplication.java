9c8fe80b31ffa5dd94e2f367c3d3a7f016c63d22
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 22:17:10 +0100

    Revert "Add orientation package"
    This reverts commit 6ee493e10c5e23cfdbacb606f383d3f781523ba0.

--- app/android/app/src/main/java/com/swifty/MainApplication.java
+++ app/android/app/src/main/java/com/swifty/MainApplication.java
@@ -3,7 +3,6 @@ package com.swifty;
 import android.app.Application;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactApplication;
-import com.github.yamill.orientation.OrientationPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new OrientationPackage(),
                     new SvgPackage(),
                     new VectorIconsPackage(),
                     new RNSpinkitPackage(),
