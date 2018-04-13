0f002a6c4237e4afdb202d868efaaf92377efaf2
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 02:39:42 +0100

    Fix indentation in SignIn styles

--- app/src/screens/SignIn/styles.js
+++ app/src/screens/SignIn/styles.js
@@ -6,17 +6,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FDFFFF',
-    
+
   },
-    signIn:{
-      textAlign:'center', 
-   fontWeight: 'bold',
-        fontSize: 30,
-        marginBottom: 30,
-        color:'#222222',
+  signIn: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 30,
+    color: '#222222',
   },
   shape: {
-  width: 0,
+    width: 0,
     height: 0,
     backgroundColor: 'transparent',
     borderStyle: 'solid',
@@ -24,9 +24,10 @@ const styles = StyleSheet.create({
     borderTopWidth: 100,
     borderRightColor: 'transparent',
     borderTopColor: '#00A7E1',
-  
-},shape2:{
-  width: 0,
+
+  },
+  shape2: {
+    width: 0,
     height: 0,
     backgroundColor: 'transparent',
     borderStyle: 'solid',
@@ -35,36 +36,38 @@ const styles = StyleSheet.create({
     borderLeftColor: 'transparent',
     borderRightColor: 'transparent',
     borderBottomColor: '#00A7E1',
-    flex:1, bottom: 0,
-    right: 0
-},
-circle: {
+    flex: 1,
+    bottom: 0,
+    right: 0,
+  },
+  circle: {
     width: 75,
     height: 75,
-    borderRadius: 100/2,
+    borderRadius: 100 / 2,
     backgroundColor: '#F2994A',
-        shadowColor: 'black',
+    shadowColor: 'black',
     shadowOffset: {
       width: 0,
       height: 4,
     },
     shadowRadius: 4,
-shadowOpacity: 0.25,
-  elevation: 5,
-  alignItems:'center',
-    justifyContent:'center',
-},
-social:{
-  flex:1,
-  justifyContent: 'center',
-  marginTop:90,
-  flexDirection: 'row',
-   alignItems:'center',
-}, circleLeft: {
-  marginRight: 50,
-}
-    
-    
+    shadowOpacity: 0.25,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  social: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 90,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  circleLeft: {
+    marginRight: 50,
+  },
+
+
 });
 
 export default styles;
