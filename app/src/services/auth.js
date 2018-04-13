db6614858639b0fe48664b33f036ab542b7a4908
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 01:36:59 +0100

    Simplify auth services

--- app/src/services/auth.js
+++ app/src/services/auth.js
@@ -1,20 +1,6 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 
-export const register = async (form) => {
-  try {
-    const response = await axios.post(`${Config.HOST}/api/users`, form);
-    console.log(response);
-  } catch (error) {
-    console.warn(error);
-  }
-}
+export const register = form => axios.post(`${Config.HOST}/api/users`, form);
 
-export const login = async (form) => {
-  try {
-    const response = await axios.post(`${Config.HOST}/api/users`, form);
-    console.log(response);
-  } catch (error) {
-    console.warn(error);
-  }
-}
+export const login = form => axios.post(`${Config.HOST}/api/auth/login`, form);
