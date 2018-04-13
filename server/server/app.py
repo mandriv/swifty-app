7917aba758e98eec94e76bffde5de8a2f43c71f5
Martin Kollie <martinkollie@gmail.com>
2018-04-13 05:16:26 +0100

    Better error messages

--- server/server/app.py
+++ server/server/app.py
@@ -5,6 +5,5 @@ from server import auth_routes
 from server import stat_routes
 
 if __name__ == '__main__':
-    print(os.getenv('PORT', 5000))
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
