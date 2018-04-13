3bad15e8b81310372db6b903a7dbad2c81ad1ce8
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 06:57:23 +0100

    Add not working /auth service

--- app/src/services/auth.js
+++ app/src/services/auth.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 import Config from 'react-native-config';
 
 export const register = form => axios.post(`${Config.HOST}/api/users`, form);
-
 export const login = form => axios.post(`${Config.HOST}/api/auth/login`, form);
+export const auth = token => axios.post(`${Config.HOST}/api/auth`, {
+  headers: {
+    Authentication: `Bearer ${token}`,
+  },
+});
