fbee94f738eaea98b05b65b051aa4cbbba28dfb9
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-13 04:50:06 +0100

    Add chart related dependencies

--- app/android/app/src/main/java/com/swifty/MainApplication.java
+++ app/android/app/src/main/java/com/swifty/MainApplication.java
@@ -3,6 +3,7 @@ package com.swifty;
 import android.app.Application;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new SvgPackage(),
             new VectorIconsPackage(),
             new RNSpinkitPackage(),
             new LinearGradientPackage(),
