3b2758a76b0fd610c68f2e1469e6ed833882975a
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-14 22:15:10 +0100

    Simplified updateStats service

--- app/src/services/fitnessData.js
+++ app/src/services/fitnessData.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 
-export const updateStepsAndCalories = (userId, token, data) =>
+export const updateStats = (userId, token, data) =>
   axios.put(`${Config.HOST}/api/stats/${userId}`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-export const incrementDistance = (userId, distanceInc) =>
-  axios.put(`${Config.HOST}/api/stats/${userId}/increment`, {
-    distance: distanceInc,
-  });
+
+export default updateStats;
