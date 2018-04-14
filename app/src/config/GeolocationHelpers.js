4bda06b242cd9beb0b8e4ec90505710a6dc4077f
mandriv <wojciech.cichoradzki@gmail.com>
2018-04-14 22:15:22 +0100

    Add geolocation!

---
+++ app/src/config/GeolocationHelpers.js
@@ -0,0 +1,76 @@
+import { AsyncStorage } from 'react-native';
+import moment from 'moment';
+
+export const getTodaysDistance = async () => {
+  try {
+    const dataStr = await AsyncStorage.getItem('geolocation');
+    if (dataStr === null) return 0;
+    const data = JSON.parse(dataStr);
+    console.log(data); // eslint-disable-line
+    const yesterdayLastLoc = getYesterdaysLastLocation(data);
+    const mostRecentLoc = getMostRecentLocation(data);
+    if (yesterdayLastLoc === null && mostRecentLoc === null) {
+      return 0;
+    }
+    if (yesterdayLastLoc === null) {
+      return mostRecentLoc.distance;
+    }
+    return mostRecentLoc.distance - yesterdayLastLoc.distance;
+  } catch (err) {
+    console.log(err); // eslint-disable-line
+    return 0;
+  }
+};
+
+export const getTodaysAverageSpeed = async () => {
+  try {
+    const dataStr = await AsyncStorage.getItem('geolocation');
+    console.log(dataStr);
+    if (dataStr === null) return 0;
+    const data = JSON.parse(dataStr);
+    console.log(data);
+    const todays = getTodaysLocations(data);
+    if (todays.length === 0) return 0;
+    const total = todays.reduce((acc, loc) => acc + loc.speed, 0);
+    return total / todays.length;
+  } catch (err) {
+    console.log(err); // eslint-disable-line
+    return 0;
+  }
+};
+
+export const getMostRecentLocation = (locations) => {
+  let mostRecentLoc = null;
+  let currentRecent = moment('01/01/1900', 'DD/MM/YYYY');
+  // eslint-disable-next-line
+  for (const loc of locations) {
+    const momentDate = moment(loc.timestamp);
+    if (momentDate.isAfter(currentRecent)) {
+      mostRecentLoc = loc;
+      currentRecent = momentDate;
+    }
+  }
+  return mostRecentLoc;
+};
+
+export const getYesterdaysLastLocation = (locations) => {
+  const format = 'DD/MM/YYYY';
+  const todayDate = moment().format(format);
+  const todayMoment = moment(todayDate, format);
+  const yesterdayMoment = todayMoment.subtract(1, 'day');
+
+  let yesterdayLastLoc = null;
+  let currentMoment = yesterdayMoment;
+  // eslint-disable-next-line
+  for (const loc of locations) {
+    const momentDate = moment(loc.timestamp);
+    if (momentDate.isAfter(currentMoment) && momentDate.isBefore(todayMoment)) {
+      yesterdayLastLoc = loc;
+      currentMoment = momentDate;
+    }
+  }
+  return yesterdayLastLoc;
+};
+
+export const getTodaysLocations = locations =>
+  locations.filter(loc => moment(loc.timestamp).isSame(moment(), 'day'));
--- app/src/services/sync.js
+++ app/src/services/sync.js
@@ -1,24 +1,28 @@
-import AppleHealthKit from 'rn-apple-healthkit';
-import { Platform } from 'react-native';
-import * as HealthData from '../config/HealthData';
-import { updateStepsAndCalories } from './fitnessData';
+import { store } from '../redux/store';
 
-const syncStepsAndCalories = async (userId, token) => {
-  // get steps and calories
+import * as HealthData from '../config/HealthData';
+import * as Geo from '../config/GeolocationHelpers';
+import { updateStats } from './fitnessData';
+
+export const syncData = async () => {
   try {
+    const distance = await Geo.getTodaysDistance();
+    const speed = await Geo.getTodaysAverageSpeed();
     const steps = await HealthData.getTodaysSteps();
     const calories = steps / HealthData.STEPS_PER_CALORIE;
-    const response = await updateStepsAndCalories(userId, token, {
+    // get token and user
+    const state = store.getState();
+    const { token, user } = state;
+    const response = await updateStats(user.id, token, {
       steps,
       calories,
+      distance,
+      average_speed: speed,
     });
-    console.log(response);
-  } catch (error) {
-    console.log(error.request);
-    console.log(error.response);
-    console.log(error);
+    console.log(response); // eslint-disable-line
+  } catch (err) {
+    console.log(err);
   }
+};
 
-}
-
-export default syncStepsAndCalories;
+export default syncData;
