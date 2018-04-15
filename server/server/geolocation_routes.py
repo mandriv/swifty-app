d753420e6913d93e26a6686c94638db2f47cfba4
Sebestyén Bence <sebestyen.bence.125@gmail.com>
2018-04-15 07:07:18 +0000

    authentication added

--- server/server/geolocation_routes.py
+++ server/server/geolocation_routes.py
@@ -2,11 +2,21 @@ from sqlalchemy import and_
 
 from server import app, db
 from flask import jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from datetime import datetime, date
 from server.Geolocation import Geolocation
+from server.User import User, ROLE_USER
 
 @app.route('/api/geolocation/<int:user_id>', methods=['GET'])
+@jwt_required
 def get_today_positions(user_id):
+
+    current_user = User.query.get(get_jwt_identity())
+
+    if (current_user.role == ROLE_USER
+            and not current_user.id == user_id):
+        return jsonify(msg="only admin can update other users"), 400
+
 
     today_date = datetime.now()
     today_locations = Geolocation.query\
@@ -22,7 +32,14 @@ def get_today_positions(user_id):
     return jsonify(locations = json_locaitons)
 
 @app.route('/api/geolocation/<int:user_id>', methods=['PUT'])
+@jwt_required
 def update_locaton(user_id):
+
+    current_user = User.query.get(get_jwt_identity())
+
+    if (current_user.role == ROLE_USER
+            and not current_user.id == user_id):
+        return jsonify(msg="only admin can update other users"), 400
 
     location_info = request.get_json()
 
